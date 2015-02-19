354b96f0ff12e4c24f1f90c7fa9cdcd11f0139f0
Roman Koshelev <romakosh@gmail.com>
2015-02-19 18:02:38 +0300

    scripts

--- Research/mipsftp/scripts/upload.ps1
+++ Research/mipsftp/scripts/upload.ps1
@@ -1,5 +1,26 @@
 try
 {
+    Main
+    exit 0
+}
+catch [Exception]
+{
+    Write-Host $_.Exception.Message
+    exit 1
+}
+
+Function Main()
+{
+    Write-Host "MIP scripts"
+  
+    Upload("product")
+}
+
+
+Function Upload([string]$feed)
+{
+    Write-Host "Upload $feed"
+
     # Load WinSCP .NET assembly
     Add-Type -Path "WinSCPnet.dll"
  
@@ -11,7 +32,6 @@ try
     $sessionOptions.UserName = "cyfir"
     $sessionOptions.Password = "v^1.1#i^1#r^1#I^3#f^0#p^3#t^Ul4yX0RBNzhEMjBDMzM1RTZFNEM2RDc2RUFBMDUyNTNBMkI4I0VeMjYw"
     $sessionOptions.GiveUpSecurityAndAcceptAnySshHostKey = $true
-    #$sessionOptions.SshHostKeyFingerprint = "ssh-rsa 2048 xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx:xx"
  
     $session = New-Object WinSCP.Session
  
@@ -24,7 +44,10 @@ try
         $transferOptions = New-Object WinSCP.TransferOptions
         $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
  
-        $transferResult = $session.PutFiles("p:\Projects\spreadbot\Research\mipsftp\data\store\pruduct\Product_Single_SKU_One_Locale.zip", "/store/product/", $false, $transferOptions)
+        $sourceFiles = ".\..\data\store\$feed\*.zip"
+        $destination = "/store/$feed/"
+
+        $transferResult = $session.PutFiles($sourceFiles, $destination, $false, $transferOptions)
  
         # Throw on any error
         $transferResult.Check()
@@ -38,15 +61,10 @@ try
     finally
     {
         # Disconnect, clean up
-        Write-Host "OK"
-
         $session.Dispose()
     }
- 
-    exit 0
 }
-catch [Exception]
-{
-    Write-Host $_.Exception.Message
-    exit 1
-}
+
+
+
+
