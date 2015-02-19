d5eaefeb4e821b7f9bf0651a39187d78cf0e17f8
Roman Koshelev <romakosh@gmail.com>
2015-02-19 20:27:12 +0300

    globals

--- Research/mipsftp/scripts/mip.globals.ps1
+++ Research/mipsftp/scripts/mip.globals.ps1
@@ -1,7 +1,3 @@
 $LocalPath       = ".\..\data\store"
 $LocalSrcPath    = "$LocalPath\src"
 $LocalZipPath    = "$LocalPath\zip"
-$RemotePath      = "/store/"
-$Session         = New-Object WinSCP.Session
-$SessionOptions  = New-Object WinSCP.SessionOptions
-$TransferOptions = New-Object WinSCP.TransferOptions
