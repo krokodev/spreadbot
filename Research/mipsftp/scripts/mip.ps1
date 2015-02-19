4bcf37fb4f1a5d520b6b705e699559fb45bf7255
Roman Koshelev <romakosh@gmail.com>
2015-02-19 19:10:26 +0300

    password

---
+++ Research/mipsftp/scripts/mip.ps1
@@ -0,0 +1,70 @@
+$LocalPath       = ".\..\data\store\"
+$RemotePath      = "/store/"
+$Session         = New-Object WinSCP.Session
+$SessionOptions  = New-Object WinSCP.SessionOptions
+$TransferOptions = New-Object WinSCP.TransferOptions
+
+Function Main()
+{
+    Log "MIP scripts"
+    
+    Add-Type -Path "WinSCPnet.dll"
+
+    InitConnectionOptions
+  
+    Upload "product"
+}
+
+Function Log([string]$rec)
+{
+    Write-Host $rec
+}
+
+Function InitConnectionOptions()
+{
+    $SessionOptions.Protocol = [WinSCP.Protocol]::Sftp
+    $SessionOptions.HostName = "mip.ebay.com"
+    $SessionOptions.PortNumber = 22
+    $SessionOptions.UserName = "cyfir"
+    $SessionOptions.Password = . .\password-secret-file.ps1
+    $SessionOptions.GiveUpSecurityAndAcceptAnySshHostKey = $true
+  
+    $TransferOptions.TransferMode = [WinSCP.TransferMode]::Binary
+}
+
+
+Function Upload([string]$feed)
+{
+    Log "Upload [$feed]..."
+
+    try
+    {
+        $Session.Open($SessionOptions)
+ 
+        $sourceFiles    = "$LocalPath$feed\*.zip"
+        $destination    = "$RemotePath$feed/"
+        $transferResult = $session.PutFiles($sourceFiles, $destination, $false, $TransferOptions)
+ 
+        $transferResult.Check()
+ 
+        foreach ($transfer in $transferResult.Transfers)
+        {
+            Log "Upload of $($transfer.FileName) succeeded"
+        }
+    }
+    finally
+    {
+        $Session.Dispose()
+    }
+}
+
+try
+{
+    Main
+    exit 0
+}
+catch [Exception]
+{
+    Write-Host $_.Exception.Message
+    exit 1
+}
