b4f26920b82a876a61be51b5f7f2f1f4d8d49c03
Roman Koshelev <romakosh@gmail.com>
2015-02-27 18:17:41 +0300

    SB-30  add: Spreadbot.Core.Configuration

--- Solution/Projects/Spreadbot.Core.Tests/Properties/AssemblyInfo.cs
+++ Solution/Projects/Spreadbot.Core.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Spreadbot.Core.Mip.Tests")]
+[assembly: AssemblyTitle("Spreadbot.Core.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Spreadbot.Core.Mip.Tests")]
+[assembly: AssemblyProduct("Spreadbot.Core.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
