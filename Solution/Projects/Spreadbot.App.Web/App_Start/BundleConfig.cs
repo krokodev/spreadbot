218cd6e451ba17a157b20445a19eec89b24b9d5c
Roman Koshelev <romakosh@gmail.com>
2015-03-06 15:51:57 +0300

    SB-33  new Spreadbot.App.Web -- css

---
+++ Solution/Projects/Spreadbot.App.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace Spreadbot.App.Web
+{
+    public static class BundleConfig
+    {
+        // >> | App_Atart | BundleConfig
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // CSS
+            bundles.Add(new StyleBundle("~/css")
+                .Include("~/Content/Themes/Default/Common.css")
+                .Include("~/Content/Themes/Default/Form.css")
+                );
+        }
+    }
+}
