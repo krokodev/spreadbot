8dadafc0b32c8189dec9ce873826c839dc0d81a4
Roman Koshelev <romakosh@gmail.com>
2015-03-07 19:27:12 +0300

    SB-37  public void PublishItemOnEbay()

---
+++ Solution/Projects/Spreadbot.Core.System/Sources/Pub/IStoreTask.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Crocodev.Common.Identifier;
+
+namespace Spreadbot.Core.System
+{
+    public interface IStoreTask
+    {
+        GenericIdentifier<IChannel, Guid> ChannelId { get; set; }
+    }
+}
