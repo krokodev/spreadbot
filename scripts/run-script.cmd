f4a92ebfe20cf213b64ab1ccec0d97a6f16a1cd7
Roman Koshelev <romakosh@gmail.com>
2015-03-31 17:33:12 +0300

    SB-73 update run-up script

--- scripts/run-script.cmd
+++ scripts/run-script.cmd
@@ -1,2 +1,4 @@
 @echo off
-powershell.exe -ExecutionPolicy RemoteSigned %*
+set pspath=%windir%\Sysnative\WindowsPowerShell\v1.0
+if not exist %pspath%\powershell.exe set pspath=%windir%\System32\WindowsPowerShell\v1.0
+%pspath%\powershell.exe -ExecutionPolicy RemoteSigned %*
