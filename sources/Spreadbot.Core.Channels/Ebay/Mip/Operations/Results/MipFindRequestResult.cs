1d941596e75ff0940e8ef07c8fdada46196cb1e9
Roman Koshelev <romakosh@gmail.com>
2015-04-01 21:07:02 +0300

    SB-75 ref: Remove constructors MipResponse, use { initialization }

---
+++ sources/Spreadbot.Core.Channels/Ebay/Mip/Operations/Results/MipFindRequestResult.cs
@@ -0,0 +1,12 @@
+﻿// Spreadbot (c) 2015 Crocodev
+// Spreadbot.Core.Channels
+// MipFindRequestResult.cs
+// Roman, 2015-04-01 7:54 PM
+
+namespace Spreadbot.Core.Channels.Ebay.Mip.Operations.Results
+{
+    public class MipFindRequestResult : AbstractMipResponseResult {
+        public string RemoteFileName { get; set; }
+        public string RemoteDir { get; set; }
+    }
+}
